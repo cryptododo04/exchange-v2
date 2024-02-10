9f60a72d0189ca6a37f51aac86e4f550f53063fc
santi cuda <santiagoacuda@gmail.com>
2024-02-10 19:32:28 -0300

    added destiny money options on its tag

--- main.js
+++ main.js
@@ -21,3 +21,19 @@ Peru
 Republica Dominicana
 Venezuela
 */
+
+/*
+•  Colombia (COP) :  “BANCOLOMBIA”, “NEQUI”
+•  Chile (CLP) :           “ BANCO BCI” , “RUT “
+•  Argentina (ARS):    “TRASNFERENCIAS, “MERCADOPAGO”
+•  Mexico (MXN):    “BANAMEX”, “7ELEVEN”, “MERCADOPAGO”
+•  Peru (SOL):            “TRANSFERENCIAS”, “YAPE”
+•  Ecuador (USD):      “BANCO PICHINCHA”   ,  “ BANCO PACIFICO  “
+•  Brasil                        “PIX”
+•  Venezuela        “Banesco”   ,  ”pago móvil”
+•  Costa rica        “Banco de Costa rica”
+•  España   :       “Bizum”  "Transferencias"
+•  Estados Unidos  “zelle” "cashapp" "venmo"   (20% de comision)
+• republica dominicana "banreservas"
+
+*/
