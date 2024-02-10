d8f1b55ea1b5bbc6e33e18ba47bb916ff5874003
santi cuda <santiagoacuda@gmail.com>
2024-02-10 17:37:22 -0300

    overhaul completo

---
+++ main.js
@@ -0,0 +1,19 @@
+function selectCountry(country) {
+    document.getElementById("countryDropdown").innerText = country;
+}
+
+
+  /*
+Argentina
+Brasil
+Chile
+Colombia
+Costa Rica
+Ecuador
+España
+Estados Unidos
+Mexico
+Peru
+Republica Dominicana
+Venezuela
+*/
