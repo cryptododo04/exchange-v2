dc820352bb516afa0af2a2f9d643efa9599d7cc0
santi cuda <santiagoacuda@gmail.com>
2024-02-11 09:23:42 -0300

    added new icons reorganize the interface order

--- main.js
+++ main.js
@@ -1,44 +1,75 @@
 
-function selectCountryOrigen(country) {
-    document.getElementById("countryDropdownOrigen").innerText = country;
-    let imageUrl = obtenerUrlImagen(country);
-    document.querySelector(".origen-imagen img").src = imageUrl;
-}
+    function selectCountryOrigen(country) {
+        document.getElementById("countryDropdownOrigen").innerText = country;
+        let imageUrl = obtenerUrlImagen(country);
+        let imagen = document.querySelector(".origen-imagen img");
+    
+        // Agregar clase de animación
+        imagen.classList.add("slide-in-out");
+    
+        setTimeout(() => {
+            // Cambiar la URL de la imagen después de que termine la animación
+            imagen.src = imageUrl;
+    
+            // Activar la clase de animación
+            imagen.classList.add("active");
+    
+            // Eliminar clase de animación después de un breve retraso
+            setTimeout(() => {
+                imagen.classList.remove("slide-in-out", "active");
+            }, 100); // Duración de la animación
+        }, 10); // Retraso antes de cambiar la URL
+    }
 
 
-
-function selectCountryDestino(country2) {
-    document.getElementById("countryDropdownDestino").innerText = country2;
+    function selectCountryDestino(country2) {
+        document.getElementById("countryDropdownDestino").innerText = country2;
     let imageUrl = obtenerUrlImagen(country2);
-    document.querySelector(".destino-imagen img").src = imageUrl;
+    let imagen = document.querySelector(".destino-imagen img");
+
+    // Agregar clase de animación
+    imagen.classList.add("slide-in-out");
+
+    setTimeout(() => {
+        // Cambiar la URL de la imagen después de que termine la animación
+        imagen.src = imageUrl;
+
+        // Activar la clase de animación
+        imagen.classList.add("active");
+
+        // Eliminar clase de animación después de un breve retraso
+        setTimeout(() => {
+            imagen.classList.remove("slide-in-out", "active");
+        }, 500); // Duración de la animación
+    }, 50); // Retraso antes de cambiar la URL
 }
 
-function obtenerUrlImagen(countryName) {
-    let imageUrls = {
-        'Argentina (ARS)': 'images/argentina.png',
-        'Brasil (R$)': 'images/brazil.png',
-        'Chile (CLP)': 'images/chile.png',
-        'Chile (CLP) RUT': 'images/chile.png', // Reutilizando la misma imagen que para 'Chile (CLP)'
-        'Colombia (COP)': 'images/colombia.png',
-        'Costa Rica (CRC)': 'images/costa-rica.png',
-        'Ecuador (USD)': 'images/ecuador.png',
-        'España (EUR)': 'images/spain.png',
-        'Estados Unidos (USD): (20% de comision)': 'images/usa.png',
-        'Estados Unidos (USD)': 'images/usa.png',
-        'Mexico (MXN)': 'images/mexico.png',
-        'Peru (SOL)': 'images/peru.png',
-        'Republica Dominicana (POP)': 'images/dominican-republic.png',
-        'Venezuela (VES)': 'images/venezuela.png',
-        'Paypal (USD)': 'images/paypal.png',
-        'Prex (ARS-USD)': 'images/prex.png',
-        'Revolut (USD)': 'images/revolut.png',
-        'Skrill (USD)': 'images/skrill.png',
-        'Bitcoin (BTC)': 'images/bitcoin.png',
-        'Ethereum (ETH)': 'images/ethereum.png',
-        'USDT (USDT)': 'images/usdt.png'
-    };
-    return imageUrls[countryName] || ''; // Si no se encuentra la URL, devuelve una cadena vacía
-}
+    function obtenerUrlImagen(countryName) {
+        let imageUrls = {
+            'Argentina (ARS)': 'images/argentina.png',
+            'Brasil (R$)': 'images/brazil.png',
+            'Chile (CLP)': 'images/chile.png',
+            'Chile (CLP) RUT': 'images/chile.png', // Reutilizando la misma imagen que para 'Chile (CLP)'
+            'Colombia (COP)': 'images/colombia.png',
+            'Costa Rica (CRC)': 'images/costa-rica.png',
+            'Ecuador (USD)': 'images/ecuador.png',
+            'España (EUR)': 'images/spain.png',
+            'Estados Unidos (USD): (20% de comision)': 'images/usa.png',
+            'Estados Unidos (USD)': 'images/usa.png',
+            'Mexico (MXN)': 'images/mexico.png',
+            'Peru (SOL)': 'images/peru.png',
+            'Republica Dominicana (POP)': 'images/dominican-republic.png',
+            'Venezuela (VES)': 'images/venezuela.png',
+            'Paypal (USD)': 'images/paypal.png',
+            'Prex (ARS-USD)': 'images/prex.png',
+            'Revolut (USD)': 'images/revolut.png',
+            'Skrill (USD)': 'images/skrill.png',
+            'Bitcoin (BTC)': 'images/bitcoin.png',
+            'Ethereum (ETH)': 'images/ethereum.png',
+            'USDT (USDT)': 'images/usdt.png'
+        };
+        return imageUrls[countryName] || ''; // Si no se encuentra la URL, devuelve una cadena vacía
+    }
 
   /*
 Argentina
